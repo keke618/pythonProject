76805db3dc98f3fd94dfb7138f002de4ce340db2
xxx <w2371090934@gmail>
2021-09-01 20:17:17 +0800

    Default Changelist

---
+++ lab2-2.py
@@ -0,0 +1,6 @@
+import random
+
+name = ['万前','梁栋','张毅','李政','周煜林','谢雄鑫']
+
+win = random.choice(name)
+print(f"恭喜{win}!你中了一个没有奖金的大奖！")
---
+++ lab2-3.py
@@ -0,0 +1,16 @@
+import random
+
+num_list = []
+for figure in range(1,101):
+    num = random.randint(1,100)
+    num_list.append(num)
+
+print("生成的100个整数是：",num_list)
+fig = 0
+for i in num_list:
+    fig = fig + i
+print("100个整数之和",fig)
+
+print("100个整数中最大值",max(num_list))
+print("100个整数中最小值",min(num_list))
+
