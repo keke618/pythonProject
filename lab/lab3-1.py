495ec61c00bc09d5054e13aec351ff5e88861bb6
xxx <w2371090934@gmail>
2021-09-08 11:31:21 +0800

    Default Changelist

--- lab/lab3-1.py
+++ lab/lab3-1.py
@@ -4,19 +4,27 @@ import re
 
 mac = '166 54a2.74f7.0326 DYNAMIC Gi1/0/11'
 
-vlanid_list = re.findall('166',mac)
+vlanid = re.search('(\d+)',mac)
+print('VLAN ID    : ', vlanid.group())
 
-for vlanid in vlanid_list:
-    print('VLAN ID    :',vlanid)
+mac1 = re.search('\s(.*?)\s',mac)
+print('MAC        :', mac1.group())
 
-mac_list = re.findall('\s(.*?)\s',mac)
-for m in mac_list:
-    print('MAC        :',m)
+type = re.search('[D].*[C]',mac)
+print('Type       : ', type.group())
 
-type_list = re.findall('[D].*[C]',mac)
-for t in type_list:
-    print('Type       :',t)
+interface = re.search('[G].*',mac)
+print('Interface  : ', interface.group())
 
-Interface_list = re.findall('[G].*',mac)
-for interface in Interface_list:
-    print('Interface  :',interface)
+
+# mac_list = re.findall('\s(.*?)\s',mac)
+# for m in mac_list:
+#     print('MAC        :',m)
+#
+# type_list = re.findall('[D].*[C]',mac)
+# for t in type_list:
+#     print('Type       :',t)
+#
+# Interface_list = re.findall('[G].*',mac)
+# for interface in Interface_list:
+#     print('Interface  :',interface)
