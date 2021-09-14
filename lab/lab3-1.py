2ddda423d25bedd7bde0b03cdac9f33efd0d7146
xxx <w2371090934@gmail>
2021-09-14 09:14:01 +0800

    Default Changelist

--- lab/lab3-1.py
+++ lab/lab3-1.py
@@ -4,17 +4,17 @@ import re
 
 mac = '166 54a2.74f7.0326 DYNAMIC Gi1/0/11'
 
-vlanid = re.search('(\d+)',mac)
+vlanid = re.search('(\d{1,4})',mac)
 print('VLAN ID    : ', vlanid.group())
 
 # mac1 = re.search('\s(.*?)\s',mac)
 mac1 = re.search('([0-9|a-f]{4}.){2}[0-9|a-f]{4}', mac)
 print('MAC        : ', mac1.group())
 
-type = re.search('[D].*[C]',mac)
+type = re.search('\w+',mac)
 print('Type       : ', type.group())
 
-interface = re.search('[G].*',mac)
+interface = re.search(r'(\w+/)(\d+)(/\d+)', mac)
 print('Interface  : ', interface.group())
 
 
