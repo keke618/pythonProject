b05298fc6585900d2230a04dbedc4553231fb0e1
xxx <w2371090934@gmail>
2021-09-14 08:51:49 +0800

    Default Changelist

--- lab/lab4-2.py
+++ lab/lab4-2.py
@@ -1,3 +1,6 @@
 port_list = ['eth 1/101/1/42', 'eth 1/101/1/26', 'eth 1/101/1/23', 'eth 1/101/1/7', 'eth 1/101/2/46', 'eth 1/101/1/34', 'eth 1/101/1/18', 'eth 1/101/1/13', 'eth 1/101/1/32', 'eth 1/101/1/25', 'eth 1/101/1/45', 'eth 1/101/2/8']
-list = []
 
+new = sorted(port_list, key = lambda i:int(i.split('/')[-1]))
+new = sorted(new, key = lambda i:int(i.split('/')[-2]))
+
+print(new)
