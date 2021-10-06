be3d4e9707a1a3d75b5a3f819d707096b83c1a3f
xxx <w2371090934@gmail>
2021-10-06 08:28:58 +0800

    .

--- lab/lab9-2&3.py
+++ lab/lab9-2&3.py
@@ -5,29 +5,18 @@
 # @File : lab9-2&3.py 
 # @Software: PyCharm
 import paramiko
-import time
 
 
-def ssh_router(ip, username, passwd, cmd):
-    ssh = paramiko.SSHClient()                      # 创建SSH Client
-    ssh.load_system_host_keys()                     # 加载系统SSH密钥
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())   # 添加新的SSH密钥
-    try:
-        ssh.connect(ip, username=username, password=passwd, timeout=5)       # SSH连接
-    except Exception:
-        print('无法登录到路由器，请检查配置!')
-    else:
-        shell = ssh.invoke_shell()      # 激活交互式shell
-        shell.send(cmd + '\n')          # 发送命令
-        time.sleep(1)
-        result = shell.recv(1024).decode()  # 获取路由器返回的信息
-        return result
-    ssh.close()
+def xxx_ssh(ip, username, port, passwd, cmd):
+    ssh = paramiko.SSHClient()
+    ssh.load_system_host_keys()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(ip, port=port, username=username, password=passwd, timeout=5, compress=True)
+    stdin, stdout, stderr = ssh.exec_command(cmd)
+    x = stdout.read().decode()
+    return x
 
 
 if __name__ == '__main__':
-    ip = '192.168.80.110'
-    username = 'user-ssh'
-    passwd = 'huawei123'
-    cmd = 'dis ip int br'
-    print(ssh_router(ip, username, passwd, cmd))
+    print(xxx_ssh('192.168.80.10', 'root', '22', 'redhat', 'ls'))
+    print(xxx_ssh('192.168.80.10', 'root', '22', 'redhat', 'pwd'))
